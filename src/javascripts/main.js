6b2d95b0d32a97078299d1472128d43226d900c6
Kenny Mc <kenymc1@gmail.com>
2020-01-03 19:27:07 -0600

    worked on buttons

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -3,20 +3,20 @@ import '../styles/main.scss';
 
 console.error('Hello world!');
 
-const pictures = [
+const allPictures = [
   {
     image: 'https://images.pexels.com/photos/3377538/pexels-photo-3377538.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     id: '1',
     color: 'full',
-    direction: 'landscape',
+    orientation: 'portrait',
     occasion: 'trip',
-    location: 'turkey',
+    location: 'california',
   },
   {
     image: 'https://images.pexels.com/photos/1953451/pexels-photo-1953451.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     id: '2',
     color: 'full',
-    direction: 'landscape',
+    orientation: 'portrait',
     occasion: 'wedding',
     location: 'california',
   },
@@ -24,23 +24,23 @@ const pictures = [
     image: 'https://images.pexels.com/photos/1684915/pexels-photo-1684915.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     id: '3',
     color: 'full',
-    direction: 'portrait',
+    orientation: 'landscape',
     occasion: 'trip',
-    location: 'california',
+    location: 'turkey',
   },
   {
     image: 'https://images.pexels.com/photos/2822949/pexels-photo-2822949.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     id: '4',
     color: 'b/w',
-    direction: 'landscape',
-    occasion: 'trip',
-    location: 'studio',
+    orientation: 'portrait',
+    occasion: 'studio',
+    location: 'california',
   },
   {
     image: 'https://images.pexels.com/photos/3381028/pexels-photo-3381028.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     id: '5',
     color: 'full',
-    direction: 'landscape',
+    orientation: 'portrait',
     occasion: 'trip',
     location: 'newYork',
   },
@@ -48,7 +48,7 @@ const pictures = [
     image: 'https://images.pexels.com/photos/1137745/pexels-photo-1137745.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260/',
     id: '6',
     color: 'full',
-    direction: 'landscape',
+    orientation: 'landscape',
     occasion: 'trip',
     location: 'newYork',
   },
@@ -56,7 +56,7 @@ const pictures = [
     image: 'https://images.pexels.com/photos/245535/pexels-photo-245535.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     id: '7',
     color: 'full',
-    direction: 'landscape',
+    orientation: 'portrait',
     occasion: 'trip',
     location: 'turkey',
   },
@@ -64,15 +64,15 @@ const pictures = [
     image: 'https://images.pexels.com/photos/911738/pexels-photo-911738.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     id: '8',
     color: 'b/w',
-    direction: 'portrait',
-    occasion: 'trip',
-    location: 'turkey',
+    orientation: 'landscape',
+    occasion: 'studio',
+    location: 'california',
   },
   {
-    image: 'https://images.pexels.com/photos/3359742/pexels-photo-3359742.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    image: 'https://images.pexels.com/photos/3494648/pexels-photo-3494648.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     id: '9',
     color: 'full',
-    direction: 'landscape',
+    orientation: 'portrait',
     occasion: 'trip',
     location: 'turkey',
   },
@@ -80,15 +80,15 @@ const pictures = [
     image: 'https://images.pexels.com/photos/3354675/pexels-photo-3354675.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     id: '10',
     color: 'full',
-    direction: 'landscape',
-    occasion: 'trip',
+    orientation: 'portrait',
+    occasion: 'studio',
     location: 'studio',
   },
   {
     image: 'https://images.pexels.com/photos/3373054/pexels-photo-3373054.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     id: '11',
     color: 'full',
-    direction: 'portrait',
+    orientation: 'landscape',
     occasion: 'trip',
     location: 'india',
   },
@@ -96,7 +96,7 @@ const pictures = [
     image: 'https://images.pexels.com/photos/1707402/pexels-photo-1707402.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     id: '12',
     color: 'full',
-    direction: 'landscape',
+    orientation: 'landscape',
     occasion: 'trip',
     location: 'india',
   },
@@ -104,7 +104,7 @@ const pictures = [
     image: 'https://images.pexels.com/photos/2774570/pexels-photo-2774570.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     id: '13',
     color: 'b/w',
-    direction: 'landscape',
+    orientation: 'landscape',
     occasion: 'trip',
     location: 'newYork',
   },
@@ -112,7 +112,7 @@ const pictures = [
     image: 'https://images.pexels.com/photos/2306920/pexels-photo-2306920.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     id: '14',
     color: 'b/w',
-    direction: 'landscape',
+    orientation: 'landscape',
     occasion: 'trip',
     location: 'newYork',
   },
@@ -120,7 +120,7 @@ const pictures = [
     image: 'https://images.pexels.com/photos/1020544/pexels-photo-1020544.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     id: '15',
     color: 'b/w',
-    direction: 'portrait',
+    orientation: 'landscape',
     occasion: 'trip',
     location: 'newYork',
   },
@@ -128,31 +128,31 @@ const pictures = [
     image: 'https://images.pexels.com/photos/3359742/pexels-photo-3359742.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     id: '16',
     color: 'full',
-    direction: 'landscape',
+    orientation: 'portrait',
     occasion: 'trip',
-    location: 'turkey',
+    location: 'california',
   },
   {
     image: 'https://images.pexels.com/photos/3257375/pexels-photo-3257375.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     id: '17',
     color: 'full',
-    direction: 'landscape',
-    occasion: 'trip',
+    orientation: 'portrait',
+    occasion: 'wedding',
     location: 'newYork',
   },
   {
     image: 'https://images.pexels.com/photos/2462402/pexels-photo-2462402.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
     id: '18',
     color: 'full',
-    direction: 'landscape',
-    occasion: 'trip',
+    orientation: 'portrait',
+    occasion: 'wedding',
     location: 'newYork',
   },
   {
     image: 'https://images.pexels.com/photos/3234167/pexels-photo-3234167.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     id: '19',
     color: 'full',
-    direction: 'landscape',
+    orientation: 'portrait',
     occasion: 'trip',
     location: 'turkey',
   },
@@ -160,7 +160,7 @@ const pictures = [
     image: 'https://images.pexels.com/photos/3361480/pexels-photo-3361480.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     id: '20',
     color: 'full',
-    direction: 'landscape',
+    orientation: 'portrait',
     occasion: 'trip',
     location: 'india',
   },
@@ -168,11 +168,13 @@ const pictures = [
     image: 'https://images.pexels.com/photos/949219/pexels-photo-949219.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     id: '21',
     color: 'full',
-    direction: 'portrait',
-    occasion: 'trip',
+    orientation: 'landscape',
+    occasion: 'wedding',
     location: 'turkey',
   },
 ];
+
+let visiblePictures = [...allPictures];
 
 const printToDom = (toPrint, divId) => {
   document.getElementById(divId).innerHTML = toPrint;
@@ -180,62 +182,96 @@ const printToDom = (toPrint, divId) => {
 
 const printCards = () => {
   let domString = '';
-  for (let i = 0; i < pictures.length; i += 1) {
+  for (let i = 0; i < visiblePictures.length; i += 1) {
     domString += `
 
     <div class="col-m d-inline-flex offset-.5">
             <div class="profile">
-              <img src=${pictures[i].image} class="images">
+              <img src=${visiblePictures[i].image} class="images">
             </div>
     </div>
           `;
     printToDom(domString, 'card-place');
   }
 };
-$('#card-place').append(printCards(pictures));
+$('#card-place').append(printCards(visiblePictures));
 
 printCards();
 
-
-// const sortCards = (e) => {
-//   const b/w = [];
-//   for (let i = 0; i < pictures.length; i += 1) {
-//     if (pictures[i].color === 'b/w') {
-//       full.push(pictures[i]);
-//       e.preventDefault();
-//     }
-//   }
-
+// const portCards = () => {
+//   console.log('port');
+//   visiblePictures = allPictures.filter((picture) => picture.orientation === 'portrait');
 //   printCards();
 // };
 
-document.getElementById('b/w').addEventListener('click', (e) => {
-  const b/w = e.target.id;
-  const blackWhite = [];
-  for (let i = 0; i < pictures.length; i += 1) {
-    const pic = pictures[i];
-    if (pic.color === 'b/w') {
-      blackWhite.push(pic);
-    }
-  }
-  printCards(blackWhite);
-});
+const landCards = () => {
+  console.log('land');
+  visiblePictures = allPictures.filter((picture) => picture.orientation === 'landscape');
+  printCards();
+};
+
+const sortCards = () => {
+  console.log('test');
+  visiblePictures = allPictures.filter((picture) => picture.color === 'b/w');
+  printCards();
+};
+const fullColorCards = () => {
+  console.log('test');
+  visiblePictures = allPictures.filter((picture) => picture.color === 'full');
+  printCards();
+};
+
+const studioCards = () => {
+  console.log('test');
+  visiblePictures = allPictures.filter((picture) => picture.occasion === 'studio');
+  printCards();
+};
+
+const tripCards = () => {
+  console.log('test');
+  visiblePictures = allPictures.filter((picture) => picture.occasion === 'trip');
+  printCards();
+};
+
+const weddingCards = () => {
+  console.log('test');
+  visiblePictures = allPictures.filter((picture) => picture.occasion === 'wedding');
+  printCards();
+};
+
+const yorkCards = () => {
+  console.log('test');
+  visiblePictures = allPictures.filter((picture) => picture.location === 'newYork');
+  printCards();
+};
+
+const californiaCards = () => {
+  console.log('test');
+  visiblePictures = allPictures.filter((picture) => picture.location === 'california');
+  printCards();
+};
+
+const indiaCards = () => {
+  console.log('test');
+  visiblePictures = allPictures.filter((picture) => picture.location === 'india');
+  printCards();
+};
+
+const turkeyCards = () => {
+  console.log('test');
+  visiblePictures = allPictures.filter((picture) => picture.location === 'turkey');
+  printCards();
+};
 
 
-// document.getElementById('full').addEventListener('click', sortCards);
-// document.getElementById('b/w').addEventListener('click', sortCards);
-// document.getElementById('port').addEventListener('click', sortCards);
-// document.getElementById('land').addEventListener('click', sortCards);
-// document.getElementById('stu').addEventListener('click', sortCards);
-// document.getElementById('trav').addEventListener('click', sortCards);
-// document.getElementById('event').addEventListener('click', sortCards);
-// document.getElementById('york').addEventListener('click', sortCards);
-// document.getElementById('stu').addEventListener('click', sortCards);
-// document.getElementById('india').addEventListener('click', sortCards);
-// document.getElementById('turk').addEventListener('click', sortCards);
-
-// $('body').on('click', '.close', () => {
-//   util.printToDom('single-card', '');
-//   multiPlanet.pic();
-//   multiPlanet.event();
-// });
+// document.getElementById('port').addEventListener('click', portCards);
+document.getElementById('land').addEventListener('click', landCards);
+document.getElementById('blackWhite').addEventListener('click', sortCards);
+document.getElementById('full').addEventListener('click', fullColorCards);
+document.getElementById('stu').addEventListener('click', studioCards);
+document.getElementById('trip').addEventListener('click', tripCards);
+document.getElementById('wedd').addEventListener('click', weddingCards);
+document.getElementById('york').addEventListener('click', yorkCards);
+document.getElementById('california').addEventListener('click', californiaCards);
+document.getElementById('india').addEventListener('click', indiaCards);
+document.getElementById('turkey').addEventListener('click', turkeyCards);
